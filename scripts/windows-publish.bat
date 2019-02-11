508087c36ac1722ae8944558218392110dcd6666
miramaris <miramaris@protonmail.com>
2019-02-11 14:04:52 -0800

    use curl for windows instead of scoop

--- scripts/windows-publish.bat
+++ scripts/windows-publish.bat
@@ -1,5 +1,6 @@
 if defined CIRRUS_RELEASE (
-    hub release edit -m "" --attach .\bin\release\netcoreapp2.1\win-x64\native\BDInfo.exe#bdinfox-windows.exe %CIRRUS_RELEASE%
+    echo Uploading asset...
+    curl -X POST --data-binary @bin\release\netcoreapp2.1\win-x64\native\BDInfo.exe --header "Authorization: token %GITHUB_TOKEN%" --header "Content-Type: application/octet-stream" "https://uploads.github.com/repos/%CIRRUS_REPO_FULL_NAME%/releases/%CIRRUS_RELEASE%/assets?name=bdinfox-windows.exe"
 ) else (
     echo Not a release. No need to deploy!
 )
