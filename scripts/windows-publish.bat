f555fca029b595e29e36dda55b315583ff75d597
miramaris <miramaris@protonmail.com>
2019-02-11 12:55:51 -0800

    rename published windows binary

--- scripts/windows-publish.bat
+++ scripts/windows-publish.bat
@@ -1,5 +1,5 @@
 if defined CIRRUS_RELEASE (
-    hub release edit -m "" --attach .\bin\release\netcoreapp2.1\win-x64\native\BDInfo.exe %CIRRUS_RELEASE%
+    hub release edit -m "" --attach .\bin\release\netcoreapp2.1\win-x64\native\BDInfo.exe#bdinfox-windows.exe %CIRRUS_RELEASE%
 ) else (
     echo Not a release. No need to deploy!
 )
